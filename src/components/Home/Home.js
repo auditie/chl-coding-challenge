681af3827d74da73f78631c3d3563ef67837158d
Audun <audunyoung@gmail.com>
2022-11-02 15:03:44 -0600

    Work on inital home upload structure.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,22 @@
 import './Home.scss';
 import { Component } from 'react';
+import cloud from '../../assets/icons/cloud.png';
 
 class Home extends Component {
     render() {
         return (
             <div className='home'>
-
+                <div className='home__upload'>
+                    <img src={cloud} alt='purple cloud' className='home__upload-cloud' />
+                    <h1 className='home__upload-title'>UPLOAD FILES</h1>
+                    <h2 className='home__upload-sub-title'>Upload documents you want to share with your team.</h2>
+                    <div className='home__upload-dragndrop'>
+                        
+                    </div>
+                </div>
+                <section className='home__uploaded'>
+                    <h2 className='home__uploaded-title'>Uploaded files</h2>
+                </section>
             </div>
         )
     }
