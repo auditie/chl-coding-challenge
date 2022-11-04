387d02853c9a89bad67a6d7f66677cd81456372a
Audun <audunyoung@gmail.com>
2022-11-04 13:02:19 -0600

    Start dragndrop component structure.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,20 @@
 import './Home.scss';
 import { Component } from 'react';
-import cloud from '../../assets/icons/cloud.png';
 import FileUpload from '../FileUpload/FileUpload';
+import uploadCloud from '../../assets/icons/cloud-upload.png';
 
 class Home extends Component {
     render() {
         return (
             <div className='home'>
                 <div className='home__upload'>
-                    <img src={cloud} alt='purple cloud' className='home__upload-cloud' />
                     <h1 className='home__upload-title'>UPLOAD FILES</h1>
                     <h2 className='home__upload-sub-title'>Upload documents you want to share with your team.</h2>
                     <div className='home__upload-dragndrop'>
-
+                        <img src={uploadCloud} alt='arrow in cloud' className='home__upload-dragndrop-icon' />
+                        <p className='home__upload-dragndrop-text'>Drag & Drop your files here</p>
+                        <p className='home__upload-dragndrop-text'>OR</p>
+                        <button className='home__upload-dragndrop-button'>Browse Files</button>
                     </div>
                 </div>
                 <section className='home__uploaded'>
